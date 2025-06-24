026eb4fa699b42a52c76b22727478ba714a8543a
Zealep <cristhianpelaez13@gmail.com>
2025-06-23 21:46:31 -0500

    datos personales otros datos

--- src/app/pacientes/paciente-informacion/datos-personales/datos-personales.component.ts
+++ src/app/pacientes/paciente-informacion/datos-personales/datos-personales.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NgLabelTemplateDirective, NgOptionTemplateDirective, NgSelectComponent } from '@ng-select/ng-select';
 import { FlatpickrDefaultsInterface, FlatpickrDirective, provideFlatpickrDefaults } from 'angularx-flatpickr';
 import { sign } from 'crypto';
 import { Spanish } from 'flatpickr/dist/l10n/es';
@@ -9,20 +10,31 @@ import { firstValueFrom } from 'rxjs';
 import { Paciente } from 'src/app/model/paciente';
 import { PacienteService } from 'src/app/service/paciente.service';
 import { ToastService } from 'src/app/service/toast.service';
+import { toIsoDate } from 'src/app/utils/convert';
 
 @Component({
     selector: 'app-datos-personales',
     standalone: true,
-    imports: [CommonModule, FormsModule, ReactiveFormsModule, FlatpickrDirective],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, FlatpickrDirective,NgLabelTemplateDirective,
+    NgOptionTemplateDirective,
+    NgSelectComponent],
      providers: [provideFlatpickrDefaults()],
     templateUrl: './datos-personales.component.html',
 })
 export class DatosPersonalesComponent {
     private readonly pacientesService = inject(PacienteService);
     private readonly fb = inject(FormBuilder);
-    private toastService = inject(ToastService);
+    private readonly toastService = inject(ToastService);
     private router = inject(Router);
     private route = inject(ActivatedRoute)
+
+    estadosCiviles = [
+  { label: 'SOLTERO', value: 'S' },
+  { label: 'CASADO', value: 'C' },
+  { label: 'DIVORCIADO', value: 'D' },
+  { label: 'VIUDO', value: 'V' }
+];
+
     paciente = signal<Paciente>({
         apellidos: '',
         nombres: '',
@@ -74,8 +86,14 @@ export class DatosPersonalesComponent {
         const paciente: Paciente = {
             ...this.paciente(),
             ...this.params.value,
-            fechaNacimiento: this.params.value.fechaNacimiento ? this.params.value.fechaNacimiento.toISOString() : '',
+            fechaNacimiento: this.params.value.fechaNacimiento ? toIsoDate(this.params.value.fechaNacimiento) : '',
         };
+
+        this.pacientesService.save(paciente)
+        .subscribe(x=>{
+            this.toastService.showMessage('Datos actualizados correctamente');
+            
+        })
 
     }
 }
--- src/app/pacientes/paciente-informacion/estilo-vida/estilo-vida.component.ts
+++ src/app/pacientes/paciente-informacion/estilo-vida/estilo-vida.component.ts
@@ -1,9 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgLabelTemplateDirective, NgOptionComponent, NgOptionTemplateDirective, NgSelectComponent } from '@ng-select/ng-select';
+import { FlatpickrDirective } from 'angularx-flatpickr';
+import { Paciente } from 'src/app/model/paciente';
+import { PacienteService } from 'src/app/service/paciente.service';
+import { ToastService } from 'src/app/service/toast.service';
 
 @Component({
-  selector: 'app-estilo-vida',
-  standalone: true,
-  imports: [],
-  templateUrl: './estilo-vida.component.html',
+    selector: 'app-estilo-vida',
+    standalone: true,
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, NgLabelTemplateDirective, NgOptionComponent, NgSelectComponent],
+
+    templateUrl: './estilo-vida.component.html',
 })
-export class EstiloVidaComponent { }
+export class EstiloVidaComponent {
+    private readonly pacientesService = inject(PacienteService);
+    private readonly fb = inject(FormBuilder);
+    private readonly toastService = inject(ToastService);
+    private router = inject(Router);
+    private route = inject(ActivatedRoute);
+    params!: FormGroup;
+
+    paciente = signal<Paciente>({
+            apellidos: '',
+            nombres: '',
+            fechaNacimiento: '',
+            sexo: '',
+            celular: '',
+            correo: '',
+        });
+
+    actividades = [
+        { label: 'Sedentario', value: '1' },
+        { label: 'Ligeramente Activo', value: '2' },
+        { label: 'Moderamente Activo', value: '3' },
+        { label: 'Muy Activo', value: '4' },
+        { label: 'Extremadamente Activo', value: '5' },
+    ];
+}
